eef9112cd85b3ecaea15657b827e4c625b2fb00b
bfelch <kagrenac@live.com>
2015-05-04 15:55:01 -0500

    quad chopter rotationess

---
+++ Assets/scripts/Copter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Copter : MonoBehaviour {
+
+    public GameObject[] rotors;
+
+    private float theta;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < rotors.Length; i++) {
+            rotors[i].transform.rotation = Quaternion.Euler(0, theta + 15f, 0);
+        }
+
+        theta = (theta + 20f) % 360f;
+	}
+}
