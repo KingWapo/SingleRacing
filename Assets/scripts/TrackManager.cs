2696be7ab13062bbf4236b314287f96173c914b6
bfelch <kagrenac@live.com>
2015-05-04 10:54:13 -0500

    spawning finish and null fixes

--- Assets/scripts/TrackManager.cs
+++ Assets/scripts/TrackManager.cs
@@ -18,15 +18,15 @@ public class TrackManager : MonoBehaviour {
         for (int i = 0; i < numRacers; i++) {
             Vector3 offset = new Vector3();
 
-            offset.x = 8 + (4 * (i / 2));
-            offset.z = 3 * ((i % 2) == 0 ? -1 : 1);
+            offset.z = 8 + (4 * (i / 2));
+            offset.x = 3 * ((i % 2) == 0 ? -1 : 1);
 
             Vector3 startPos = startPoint.transform.position + offset;
 
             if (playerPos == i) {
-                Instantiate(playerCar, startPos, Quaternion.identity);
+                Instantiate(playerCar, startPos, startPoint.transform.rotation);
             } else {
-                Instantiate(aiCar, startPos, Quaternion.identity);
+                Instantiate(aiCar, startPos, startPoint.transform.rotation);
             }
         }
 	}
